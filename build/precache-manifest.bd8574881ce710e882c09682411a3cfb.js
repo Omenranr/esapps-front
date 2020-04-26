6876bab38f1762c47e50ea0de602abe00af7033f
Omenranr <omar.rachidilalaoui@um5s.net.ma>
2020-04-26 17:13:22 +0000

    add build folder

---
+++ build/precache-manifest.bd8574881ce710e882c09682411a3cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3088cb4c3367e43a268465d239f0dd5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c90147a59f93ef71523",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "b764576504dfb17dfe4b",
+    "url": "/static/css/main.cbdf4783.chunk.css"
+  },
+  {
+    "revision": "7c90147a59f93ef71523",
+    "url": "/static/js/2.2e76f81f.chunk.js"
+  },
+  {
+    "revision": "82fd2133711d1e027fde858069bc8536",
+    "url": "/static/js/2.2e76f81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b764576504dfb17dfe4b",
+    "url": "/static/js/main.299dcbe2.chunk.js"
+  },
+  {
+    "revision": "b76030a1cd1693e80488",
+    "url": "/static/js/runtime-main.cb0002b9.js"
+  }
+]);
